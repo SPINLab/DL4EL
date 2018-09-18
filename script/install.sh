33d1fe6ee2604aff14702a4ddcf8185f6b5151b6
reinvantveer <rein.van.t.veer@geodan.nl>
2018-09-18 14:42:36 +0200

    reqs install script for ubuntu 16.04

---
+++ script/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get install -y docker-ce
+
+# python 3.6 for ubuntu 16.04 and earlier
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install -y python3.6
+
+sudo -H pip install --upgrade pip
+sudo -H pip install pipenv
