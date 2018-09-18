e5b8b3df78e63367f0dfbbd22bb9410433176951
reinvantveer <rein.van.t.veer@geodan.nl>
2018-09-18 16:15:36 +0200

    fix typo

--- data/insert_into_database.sh
+++ data/insert_into_database.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
-csvsql --db postgresql://postres@localhost:5432/geodata \
+csvsql --db postgresql://postgres@localhost:5432/geodata \
   --create-if-not-exists \
   --db-schema energy \
   --tables building_energy_performance \
   --insert woningen_07.2017-1.csv
 
-csvsql --db postgresql://postres@localhost:5432/geodata \
+csvsql --db postgresql://postgres@localhost:5432/geodata \
   --create-if-not-exists \
   --db-schema energy \
   --tables building_energy_performance \
   --insert woningen_07.2017-2.csv
 
-csvsql --db postgresql://postres@localhost:5432/geodata \
+csvsql --db postgresql://postgres@localhost:5432/geodata \
   --create-if-not-exists \
   --db-schema energy \
   --tables building_energy_performance \
   --insert woningen_07.2017-3.csv
 
-csvsql --db postgresql://postres@localhost:5432/geodata \
+csvsql --db postgresql://postgres@localhost:5432/geodata \
   --create-if-not-exists \
   --db-schema energy \
   --tables building_energy_performance \
