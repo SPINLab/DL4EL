9757dc9ad79c8ae43b40f5d01b719c257d272cd1
reinvantveer <rein.van.t.veer@geodan.nl>
2018-09-18 16:14:53 +0200

    provide user, host and port

--- data/insert_into_database.sh
+++ data/insert_into_database.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
-csvsql --db postgresql:///geodata \
+csvsql --db postgresql://postres@localhost:5432/geodata \
   --create-if-not-exists \
   --db-schema energy \
   --tables building_energy_performance \
   --insert woningen_07.2017-1.csv
 
-csvsql --db postgresql:///geodata \
+csvsql --db postgresql://postres@localhost:5432/geodata \
   --create-if-not-exists \
   --db-schema energy \
   --tables building_energy_performance \
   --insert woningen_07.2017-2.csv
 
-csvsql --db postgresql:///geodata \
+csvsql --db postgresql://postres@localhost:5432/geodata \
   --create-if-not-exists \
   --db-schema energy \
   --tables building_energy_performance \
   --insert woningen_07.2017-3.csv
 
-csvsql --db postgresql:///geodata \
+csvsql --db postgresql://postres@localhost:5432/geodata \
   --create-if-not-exists \
   --db-schema energy \
   --tables building_energy_performance \
