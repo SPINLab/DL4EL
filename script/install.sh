b773fc099d4838cef4bd18b6a3c8438de47b514f
reinvantveer <rein.van.t.veer@geodan.nl>
2018-09-28 07:41:11 +0200

    add requirements for docker-compose, pip, postgres client

--- script/install.sh
+++ script/install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+export LC_ALL=C.UTF-8 # for amazon
+
 sudo apt-get update
 sudo apt-get install -y \
     apt-transport-https \
@@ -10,12 +12,16 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get install -y docker-ce
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-compose
 
 # python 3.6 for ubuntu 16.04 and earlier
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install -y python3.6
+sudo apt-get install -y python3.6 python3-pip
 
 sudo -H pip install --upgrade pip
 sudo -H pip install pipenv
+
+# PostGreSQL client only
+sudo apt-get install -y postgresql-client
