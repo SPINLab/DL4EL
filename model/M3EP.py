db61fdb637ce733e857bb93b0d1e58b141781b1c
reinvantveer <rein.van.t.veer@geodan.nl>
2018-10-19 16:28:46 +0200

    add random noise test submodule

--- model/M3EP.py
+++ model/M3EP.py
@@ -20,106 +20,132 @@ class M3EP(nn.Module):
         super(M3EP, self).__init__()
         self.config = config
 
+        # Random noise submodule for testing purposes
+        submodule_name = 'random_noise'
+        if submodule_name in self.config['submodules']:
+            out_channels = config['submodules'][submodule_name]['output_size']
+            setattr(self, submodule_name, nn.Sequential(OrderedDict([
+                ('y_o_c_linear1', nn.Linear(1, out_channels)),
+                ('y_o_c_relu', nn.ReLU()),
+            ])))
+
         # Year of construction submodule
         submodule_name = 'year_of_construction'
-        output_size = config['submodules'][submodule_name]['output_size']
-        setattr(self, submodule_name, nn.Sequential(OrderedDict([
-            ('y_o_c_linear1', nn.Linear(1, output_size)),
-            ('y_o_c_relu', nn.ReLU()),
-        ])))
+        if submodule_name in self.config['submodules']:
+            out_channels = config['submodules'][submodule_name]['output_size']
+            setattr(self, submodule_name, nn.Sequential(OrderedDict([
+                ('y_o_c_linear1', nn.Linear(1, out_channels)),
+                ('y_o_c_relu', nn.ReLU()),
+            ])))
 
         # Registration date submodule
         submodule_name = 'registration_date'
-        output_size = config['submodules'][submodule_name]['output_size']
-        setattr(self, submodule_name, nn.Sequential(OrderedDict([
-            ('reg_date_linear1', nn.Linear(4, output_size)),
-            ('reg_date_relu', nn.ReLU()),
-        ])))
+        if submodule_name in self.config['submodules']:
+            out_channels = config['submodules'][submodule_name]['output_size']
+            setattr(self, submodule_name, nn.Sequential(OrderedDict([
+                ('reg_date_linear1', nn.Linear(4, out_channels)),
+                ('reg_date_relu', nn.ReLU()),
+            ])))
 
         # Recorded date submodule
         submodule_name = 'recorded_date'
-        output_size = config['submodules'][submodule_name]['output_size']
-        setattr(self, submodule_name,
-                nn.Sequential(OrderedDict([
-                    ('rec_date_linear1', nn.Linear(4, output_size)),
-                    ('rec_date_relu', nn.ReLU())])))
+        if submodule_name in self.config['submodules']:
+            out_channels = config['submodules'][submodule_name]['output_size']
+            setattr(self, submodule_name,
+                    nn.Sequential(OrderedDict([
+                        ('rec_date_linear1', nn.Linear(4, out_channels)),
+                        ('rec_date_relu', nn.ReLU())])))
 
         # House number submodule
         submodule_name = 'house_number'
-        output_size = config['submodules'][submodule_name]['output_size']
-        setattr(self, submodule_name,
-                nn.Sequential(OrderedDict([
-                    ('house_number_linear1', nn.Linear(2, output_size)),
-                    ('house_number_relu', nn.ReLU())])))
+        if submodule_name in self.config['submodules']:
+            out_channels = config['submodules'][submodule_name]['output_size']
+            setattr(self, submodule_name,
+                    nn.Sequential(OrderedDict([
+                        ('house_number_linear1', nn.Linear(2, out_channels)),
+                        ('house_number_relu', nn.ReLU())])))
 
         # House number addition submodule
         submodule_name = 'house_number_addition'
-        output_size = config['submodules'][submodule_name]['output_size']
-        setattr(self, submodule_name,
-                nn.Sequential(OrderedDict([
-                    ('house_number_addition_linear1', nn.Linear(self.config['vocab_len'], output_size)),
-                    ('house_number_addition_relu', nn.ReLU()),
-                    ('house_number_addition_global_average', nn.AdaptiveAvgPool1d(1))
-                ])))
+        if submodule_name in self.config['submodules']:
+            out_channels = config['submodules'][submodule_name]['output_size']
+            setattr(self, submodule_name,
+                    nn.Sequential(OrderedDict([
+                        ('house_number_addition_linear1', nn.Linear(self.config['vocab_len'], out_channels)),
+                        ('house_number_addition_relu', nn.ReLU()),
+                        ('house_number_addition_global_average', nn.AdaptiveAvgPool1d(1))
+                    ])))
 
         # Purposes submodule
         submodule_name = 'purposes'
-        output_size = config['submodules'][submodule_name]['output_size']
-        setattr(self, submodule_name,
-                nn.Sequential(OrderedDict([
-                    ('purposes_linear1', nn.Linear(11, output_size)),
-                    ('purposes_relu', nn.ReLU())
-                ])))
+        if submodule_name in self.config['submodules']:
+            out_channels = config['submodules'][submodule_name]['output_size']
+            setattr(self, submodule_name,
+                    nn.Sequential(OrderedDict([
+                        ('purposes_linear1', nn.Linear(11, out_channels)),
+                        ('purposes_relu', nn.ReLU())
+                    ])))
 
         # Postal code submodule
         submodule_name = 'postal_code'
-        output_size = config['submodules'][submodule_name]['output_size']
-        setattr(self, submodule_name,
-                nn.Sequential(OrderedDict([
-                    ('postal_code_linear1', nn.Linear(6 * self.config['vocab_len'], output_size)),
-                    ('postal_code_relu', nn.ReLU())
-                ])))
+        if submodule_name in self.config['submodules']:
+            out_channels = config['submodules'][submodule_name]['output_size']
+            setattr(self, submodule_name,
+                    nn.Sequential(OrderedDict([
+                        ('postal_code_linear1', nn.Linear(6 * self.config['vocab_len'], out_channels)),
+                        ('postal_code_relu', nn.ReLU())
+                    ])))
 
         # Geometry submodule
         submodule_name = 'geometry'
-        output_size = self.config['submodules'][submodule_name]['output_size']
-        convnet_kernel_size = self.config['submodules'][submodule_name]['cnn_kernel_size']
-        maxpool_kernel_size = self.config['submodules'][submodule_name]['maxpool_kernel_size']
-        hidden_size = self.config['submodules'][submodule_name]['hidden_size']
-        setattr(self, submodule_name,
-                nn.Sequential(OrderedDict([
-                    ('geometry_conv1d_1', nn.Conv1d(
-                        in_channels=5,
-                        out_channels=hidden_size,
-                        kernel_size=convnet_kernel_size,
-                        padding=convnet_kernel_size - 1
-                    )),
-                    ('geometry', nn.ReLU()),
-                    # ('geometry_conv1d_2', nn.Conv1d(
-                    #     in_channels=output_size,
-                    #     out_channels=output_size,
-                    #     kernel_size=convnet_kernel_size,
-                    #     padding=convnet_kernel_size - 1
-                    # )),
-                    # ('geometry', nn.ReLU()),
-                    ('geometry_maxpool', nn.MaxPool1d(kernel_size=maxpool_kernel_size)),
-                    ('geometry_conv1d_3', nn.Conv1d(
-                        in_channels=hidden_size,
-                        out_channels=output_size,
-                        kernel_size=convnet_kernel_size,
-                        padding=convnet_kernel_size - 1
-                    )),
-                    ('geometry', nn.ReLU()),
-                    ('geometry_avg_pooling', nn.AdaptiveAvgPool1d(1)),
-                ])))
+        if submodule_name in self.config['submodules']:
+            hidden_size = self.config['submodules'][submodule_name]['hidden_size']
+            out_channels = self.config['submodules'][submodule_name]['output_size']
+            output_last_dim = self.config['late_fusion']['input_size']
+            convnet_kernel_size = self.config['submodules'][submodule_name]['cnn_kernel_size']
+            maxpool_kernel_size = self.config['submodules'][submodule_name]['maxpool_kernel_size']
 
-        # Final linear late fusion submodule
-        submodule_size_sum = sum([config['submodules'][m]['output_size'] for m in config['submodules']])
+            setattr(self, submodule_name,
+                    nn.Sequential(OrderedDict([
+                        ('geometry_conv1d_1', nn.Conv1d(
+                            in_channels=5,
+                            out_channels=hidden_size,
+                            kernel_size=convnet_kernel_size,
+                            padding=convnet_kernel_size - 1
+                        )),
+                        ('geometry', nn.ReLU()),
+                        # ('geometry_conv1d_2', nn.Conv1d(
+                        #     in_channels=output_size,
+                        #     out_channels=output_size,
+                        #     kernel_size=convnet_kernel_size,
+                        #     padding=convnet_kernel_size - 1
+                        # )),
+                        # ('geometry', nn.ReLU()),
+                        ('geometry_maxpool', nn.MaxPool1d(kernel_size=maxpool_kernel_size)),
+                        ('geometry_conv1d_3', nn.Conv1d(
+                            in_channels=hidden_size,
+                            out_channels=out_channels,
+                            kernel_size=convnet_kernel_size,
+                            padding=convnet_kernel_size - 1
+                        )),
+                        ('geometry', nn.ReLU()),
+                        ('geometry_avg_pooling', nn.AdaptiveAvgPool1d(output_last_dim)),
+                    ])))
+
+        # Late fusion submodule
+        input_size = config['late_fusion']['input_size']
         late_fusion_hidden_size = config['late_fusion']['hidden_size']
         self.late_fusion = nn.Sequential(OrderedDict([
-            ('concatenated_late_fusion_linear', nn.Linear(submodule_size_sum, late_fusion_hidden_size)),
+            ('late_fusion_concatenated_linear', nn.Linear(input_size, late_fusion_hidden_size)),
             ('late_fusion_relu', nn.ReLU()),
-            ('late_fusion_output', nn.Linear(late_fusion_hidden_size, config['output_size']))
+            ('geometry_avg_pooling', nn.AdaptiveAvgPool1d(1)),
+        ]))
+
+        # Output transformation layer
+        submodule_size_sum = sum([config['submodules'][m]['output_size'] for m in config['submodules']])
+        output_size = config['late_fusion']['output_size']
+        self.output_transformation_layer = nn.Sequential(OrderedDict([
+            ('output_transformation_linear', nn.Linear(submodule_size_sum, output_size))
         ]))
 
     def get_submodule_output(self, batch, module_name):
@@ -131,11 +157,13 @@ class M3EP(nn.Module):
 
         if module_name == 'geometry':
             module_vec = module_vec.permute(0, 2, 1)
+            module_output = module(module_vec)
+            return module_output
 
         module_output = module(module_vec)
 
-        if len(module_output.shape) == 3:
-            module_output = module_output.squeeze(dim=2)
+        if len(module_output.shape) == 2:
+            module_output = module_output.unsqueeze(dim=1)
 
         # module_output.retain_grad()
         return module_output
@@ -144,11 +172,15 @@ class M3EP(nn.Module):
         # Create outputs for all the submodules and corresponding modal vectors
         submodule_outputs = tuple(self.get_submodule_output(batch, module_name)
                                   for module_name in self.config['submodules'])
+        submodule_size_sum = sum([self.config['submodules'][m]['output_size'] for m in self.config['submodules']])
 
-        for sample in submodule_outputs:
-            if not len(sample.shape) == 2:
-                raise ValueError('Wrong tensor dimension', sample)
+        for idx, sample in enumerate(submodule_outputs):
+            if not len(sample.shape) != 2:
+                raise ValueError('Wrong tensor dimension', sample.shape, 'output from submodule', idx, ':', sample)
 
+        # noinspection PyUnresolvedReferences
         concatenated = torch.cat(submodule_outputs, dim=1)
         output = self.late_fusion(concatenated)
+        output = output.view(-1, submodule_size_sum)
+        output = self.output_transformation_layer(output)
         return output
